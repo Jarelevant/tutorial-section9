aabe121b6a0d5694e42db4495c6be87f81602ad9
git.jarele <jarele.bago.pagkaliwangan@gmail.com>
2024-08-04 21:00:36 +0800

    S3 upload

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,9 +1,17 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+// const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 
 function run(){
+
+    const bucket = core.getInput('bucket', { required:true })
+    const bucketRegion = core.getInput('bucket-region', { required:true })
+    const distList = core.getInput('dist-list', { required:true })
+
+    const s3URI = `s3://${bucket}`
+    exec.exec(`aws s3 sync ${distList} ${s3URI} --region ${bucketRegion}`)
+
     core.notice('Hello from my custom Javascript')
 }
 
